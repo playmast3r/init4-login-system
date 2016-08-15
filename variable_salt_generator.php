3338e6c77d6c846efa66e229180ddbd5b4d0d678
Siddharth Mahajan <playmast3r@users.noreply.github.com>
2016-08-15 22:23:10 +0530

    Create variable_salt_generator.php
    Custom function to generate variable salt

---
+++ variable_salt_generator.php
@@ -0,0 +1,38 @@
+<?php
+//uses openssl_random_pseudo_bytes() to help create a random number between $min and $max
+function crypto_rand_secure($min, $max)
+{
+    $range = $max - $min;
+    if ($range < 1) return $min; // not so random...
+    $log = ceil(log($range, 2));
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+        $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd >= $range);
+    return $min + $rnd;
+}
+
+//getToken($length) creates a charset to use within the token and then creates a string of length $length
+function getToken($length)
+{
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet.= "0123456789";
+    $max = strlen($codeAlphabet) - 1;
+    for ($i=0; $i < $length; $i++) {
+        $token .= $codeAlphabet[crypto_rand_secure(0, $max)];
+    }
+    return $token;
+}
+
+//Example how to generate salt from above function
+$salt = getToken(20);
+
+/* New PHP 7.0 now has two new functions that can simplify the crypto_rand_secure function above 
+random_bytes($length) and random_int($min, $max)
+*/
+?>
